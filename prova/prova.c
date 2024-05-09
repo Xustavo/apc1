11fedbe775713dfcb589e4490dd10598b03142b3
GustavoXavier <157379254+Xustavo@users.noreply.github.com>
2024-05-09 00:46:47 +0000

    pratica3

--- prova/prova.c
+++ prova/prova.c
@@ -2,7 +2,17 @@
 
 int main(){
 
-  //Ainda não decidi o que fazer!
+  int opcao;
+
+  printf("---SPOTIFY---\n");
+  printf("-------------\n");
+  printf("-1.Pesquisar-\n");
+  printf("-2.Playlists-\n");
+  printf("---3.Saída---\n");
+
+  printf("Digite a opção que deseja: ");
+  int scanner = scanf("%i", &opcao);
+  
   
   return 0;
 }
